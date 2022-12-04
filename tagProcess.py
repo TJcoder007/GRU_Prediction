ff8a857208994855ac2ab2fcf22e88c9ac75b7c5
TJcoder007 <94178448+TJcoder007@users.noreply.github.com>
2022-12-04 16:48:53 +0800

    Update tagProcess.py

--- tagProcess.py
+++ tagProcess.py
@@ -11,6 +11,6 @@ def map_func(x,theta):
 predict_df = pd.read_csv(r"C:\Users\Jiwei Tu\Desktop\predict_df.csv",encoding="utf_8_sig")
 predicted_stock_price = predict_df.applymap(lambda x: map_func(x,theta))
 predicted_stock_price.to_csv(r"C:\Users\Jiwei Tu\Desktop\predict_df_01.csv",encoding="utf_8_sig")
-real_df = pd.to_csv(r"C:\Users\Jiwei Tu\Desktop\real_df.csv",encoding="utf_8_sig")
+real_df = pd.read_csv(r"C:\Users\Jiwei Tu\Desktop\real_df.csv",encoding="utf_8_sig")
 real_stock_price = real_df.applymap(lambda x: map_func(x,theta))
 real_stock_price.to_csv(r"C:\Users\Jiwei Tu\Desktop\real_df_01.csv",encoding="utf_8_sig")
